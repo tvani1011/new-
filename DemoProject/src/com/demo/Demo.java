35cdc8d97117d145fe2a57bc07e961db75a03235
tvani1011 <tvani1011@gmail.com>
2021-10-19 10:12:18 +0530

    Demo Project

---
+++ DemoProject/src/com/demo/Demo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		List<String> list =  new ArrayList<String>();
+		list.add("vani");
+		list.add("shilpa");
+		list.add("jothi");
+		System.out.println(list);
+	}
+
+}
